8893a3530d980d78bba0acd538a213d27f3a9d2c
dtlucho <luciano.ditomaso@gmail.com>
2021-10-14 21:37:36 -0300

    Trabajo hecho en clase.

--- App.js
+++ App.js
@@ -1,18 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import Constants from "expo-constants";
 
-import {vibrate} from './utils'
+import { vibrate } from './utils'
+
+let interval;
 
 export default function App() {
+  const [running, setRunning] = useState(false);
+  const [inSession, setInSession] = useState(true);
+  const [session, setSession] = useState(25);
+  const [rest, setRest] = useState(5);
+  const [time, setTime] = useState(session * 60);
+
+  useEffect(() => { }, [])
+
+  useEffect(() => {
+    console.log('pressed')
+    if (running) {
+      interval = setInterval(() => {
+        setTime(time => time - 1)
+      }, 1000);
+    } else {
+      clearInterval(interval)
+    }
+  }, [running]);
+
+  useEffect(() => {
+    console.log(time)
+    if (time === 0) {
+      setTime(() => inSession ? rest * 60 : session * 60);
+      setInSession(() => !inSession);
+      vibrate();
+    }
+  }, [time])
+
+  const secondsToMinutes = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time - minutes * 60;
+
+    function str_pad_left(string, pad, length) {
+      return (new Array(length + 1).join(pad) + string).slice(-length);
+    }
+    let finalTime = str_pad_left(minutes, '0', 2) + ':' + str_pad_left(seconds, '0', 2);
+
+    return finalTime;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Click para hacer vibrar el dispositivo!</Text>
-      <Button 
-        title="Vibracion"
-        onPress={vibrate}
-      />
-      <StatusBar style="auto" />
+      <Text style={styles.timer}>{secondsToMinutes(time)}</Text>
+      <TouchableOpacity
+        activeOpacity={0.5}
+        onPress={() => setRunning(!running)}
+        style={styles.Button}>
+        <Text>{running ? 'Detener' : 'Iniciar'}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        activeOpacity={0.5}
+        onPress={() => {
+          clearInterval(interval);
+          setRunning(false);
+          setTime(time => session * 60)
+        }}
+        style={styles.Button}>
+        <Text>Reiniciar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -23,5 +77,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
   },
+  Button: {
+    alignItems: 'center',
+    borderLeftWidth: 1,
+    borderColor: '#000',
+    backgroundColor: 'green',
+    color: '#fff',
+    borderRadius: 5,
+    margin: 50,
+    padding: 10,
+  },
+  timer: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  }
 });
